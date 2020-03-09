93e37c404aaa8c9b595f6bf953b4de220edb6b8c
Mladen <mpenev13@gmail.com>
2020-03-10 01:28:20 +0200

    Test

---
+++ HairDresser/HairDresser/Controllers/ClientController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace HairDresser.Controllers
+{
+    public class ClientController :Controller
+    {
+
+
+    }
+}
---
+++ HairDresser/HairDresser/Models/Reservation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HairDresser.Models
+{
+    public class Reservation
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime ReservationDate { get; set; }
+
+
+
+        [ForeignKey("Client")]
+        public virtual int ClientId {get; set;}
+        public virtual Client Client { get; set; }
+
+
+    }
+}
